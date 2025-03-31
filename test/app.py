8ff1b569d4dc0cbeba707c5b0a64f2a961827c69
Martin Kirkegaard <tjuppen100@gmail.com>
2025-03-31 12:57:43 +0200

    We can extract color of bounding box to python

--- test/app.py
+++ test/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+def print_color(color):
+    return print(color)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# New endpoint to receive the color from the frontend
+@app.route('/print_color', methods=['POST'])
+def receive_color():
+    data = request.get_json()
+    color = data.get('color', None)
+    print_color(color)
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
     app.run(debug=True)
